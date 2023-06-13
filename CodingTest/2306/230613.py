4c69913e1e47f6784eea7015b293227773a38eb9
kimkihyun1 <rlarlgus0255@naver.com>
2023-06-13 22:54:23 +0900

    stack

---
+++ CodingTest/2306/230613.py
@@ -0,0 +1,19 @@
+'''
+    프로그래머스 : 주식 가격
+'''
+
+def solution(prices):
+    length = len(prices)
+    answer = [ i for i in range (length - 1, -1, -1)]
+    
+    stack = [0]
+    for i in range (1, length, 1):
+        while stack and prices[stack[-1]] > prices[i]:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+    return answer
+
+prices = [1, 2, 3, 2, 3]
+print(solution(prices))
+# [4, 3, 1, 1, 0]
