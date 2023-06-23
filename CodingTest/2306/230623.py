a31858148e1f43b07df3f8ca5f894d3070f7e303
kimkihyun1 <rlarlgus0255@naver.com>
2023-06-24 02:07:17 +0900

    투 포인터, 카카오

--- CodingTest/2306/230623.py
+++ CodingTest/2306/230623.py
@@ -1,3 +1,33 @@
 '''
     프로그래머스 : 보석 쇼핑(카카오)
 '''
+
+def solution(gems):
+    answer = [0, len(gems)]
+    size = len(set(gems))  # 보석 종류 개수
+    left, right = 0, 0
+    gem_dict = {gems[0] : 1}
+    
+    while left < len(gems) and right < len(gems):
+        if len(gem_dict) == size:
+            if right - left < answer[1] - answer[0]:
+                answer = [left, right]
+            else:
+                gem_dict[gems[left]] -= 1
+                if gem_dict[gems[left]] == 0:
+                    del gem_dict[gems[left]]
+                left += 1
+            
+        else:
+            right += 1
+            
+            if right == len(gems):
+                break
+            
+            gem_dict[gems[right]] = gem_dict.get(gems[right], 0) + 1
+    
+    return [answer[0]+1, answer[1]+1]
+
+gems = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
+print(solution(gems))
+# [3, 7]
